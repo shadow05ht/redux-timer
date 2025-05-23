b53e586adb3edca5a4bfa08a0aeb86e980a5aa3f
Shadow <togrul.huseynli.05@gmail.com>
2025-05-23 14:34:01 +0400

    last

--- src/main.jsx
+++ src/main.jsx
@@ -4,14 +4,14 @@ import './index.css'
 import App from './App'
 import { Provider } from 'react-redux'
 import store from './redux/store'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter> 
+    <HashRouter> 
       <Provider store={store}>
         <App />
       </Provider>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 )
